3c84f8d36bcd6dd02a59304f25691b20cc994722
ily-20131220 <ily20131220@gmail.com>
2024-02-27 14:50:12 +0800

    Payment title

--- pages/payment/index.js
+++ pages/payment/index.js
@@ -12,60 +12,58 @@ export default function Payment() {
 
   return (
     <>
-      <DefaultLayout title="payment">
-        <div className="row mt-5 mx-5 ">
-          <div className="col-md-8 cart-area">
-            <h3 className="bg-bg-gray-secondary rounded-3 py-3 px-4">
-              訂單明細
-            </h3>
-            <Form onSubmit={handleSubmit} method="post">
-              {/* 商品詳情 */}
-              <ProductInfo />
-              {/* 聯絡資料 */}
-              <ConnectionData />
-              {/* 付款方式 */}
-              <div>
-                <h5>
-                  <i className="bi bi-wallet-fill"></i>付款方式
-                </h5>
+      {/* <DefaultLayout title="payment"> */}
+      <div className="row mt-5 mx-5 ">
+        <div className="col-md-8 cart-area">
+          <h3 className="bg-bg-gray-secondary rounded-3 py-3 px-4">訂單明細</h3>
+          <Form onSubmit={handleSubmit} method="post">
+            {/* 商品詳情 */}
+            <ProductInfo />
+            {/* 聯絡資料 */}
+            <ConnectionData />
+            {/* 付款方式 */}
+            <div>
+              <h5>
+                <i className="bi bi-wallet-fill"></i>付款方式
+              </h5>
+            </div>
+            {/* 優惠券、點數折抵 */}
+            <PointCoupons />
+            {/* 付款類型 */}
+            <PaymentType />
+            {/* 送出資料 */}
+            <PaymentButton />
+          </Form>
+        </div>
+        <div className="col-md-4 ">
+          <div className="bg-bg-gray-secondary  rounded-4 py-3 px-4 sticky-top">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="text-truncate pe-2">
+                YOASOBI演唱會2024台北站｜YOASOBI ASIA TOUR 2023-2024 Solo
+                Concert in Taipei
               </div>
-              {/* 優惠券、點數折抵 */}
-              <PointCoupons />
-              {/* 付款類型 */}
-              <PaymentType />
-              {/* 送出資料 */}
-              <PaymentButton />
-            </Form>
-          </div>
-          <div className="col-md-4 ">
-            <div className="bg-bg-gray-secondary  rounded-4 py-3 px-4 sticky-top">
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="text-truncate pe-2">
-                  YOASOBI演唱會2024台北站｜YOASOBI ASIA TOUR 2023-2024 Solo
-                  Concert in Taipei
-                </div>
-                <span className="">$4,300.00</span>
-              </div>
-              <div className=" d-flex justify-content-between align-items-center">
-                優惠卷-全館消費95折 <span>– $300.00</span>
-              </div>
-              <div className=" d-flex justify-content-between align-items-center">
-                點數折抵 <span>– $200.00</span>
-              </div>
-              <hr />
-              <div className=" d-flex justify-content-between align-items-center fw-bold">
-                合計 <span>NT$200.00</span>
-              </div>
-              <div className=" d-flex justify-content-between align-items-center ">
-                點數回饋{' '}
-                <span>
-                  <i className="bi bi-database"></i>1136
-                </span>
-              </div>
+              <span className="">$4,300.00</span>
+            </div>
+            <div className=" d-flex justify-content-between align-items-center">
+              優惠卷-全館消費95折 <span>– $300.00</span>
+            </div>
+            <div className=" d-flex justify-content-between align-items-center">
+              點數折抵 <span>– $200.00</span>
+            </div>
+            <hr />
+            <div className=" d-flex justify-content-between align-items-center fw-bold">
+              合計 <span>NT$200.00</span>
+            </div>
+            <div className=" d-flex justify-content-between align-items-center ">
+              點數回饋{' '}
+              <span>
+                <i className="bi bi-database"></i>1136
+              </span>
             </div>
           </div>
         </div>
-      </DefaultLayout>
+      </div>
+      {/* </DefaultLayout> */}
       <style global jsx>
         {`
           body {
@@ -79,4 +77,7 @@ export default function Payment() {
       </style>
     </>
   )
+}
+Payment.getLayout = function (page) {
+  return <DefaultLayout title="Payment">{page}</DefaultLayout>
 }
