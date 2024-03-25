48906fba49b774c8fdb1fa4b49efa45ce6001ebc
keepzeal-eth <keeprealwen@gmail.com>
2024-03-25 17:10:36 +0800

    修詳情頁面

--- pages/product/[pid].js
+++ pages/product/[pid].js
@@ -208,8 +208,8 @@ export default function Detail() {
     <>
       <>
         <section>
-          {eventInfo.map((eventInfo) => (
-            <div className=" d-flex p-4 d-none d-xxl-inline-flex">
+          {eventInfo.map((eventInfo, index) => (
+            <div key={index} className=" d-flex p-4 d-none d-xxl-inline-flex">
               <p>
                 首頁 <i className="bi bi-chevron-right"></i>
               </p>
@@ -439,7 +439,7 @@ export default function Detail() {
               <section className="row">
                 {/* left bar */}
                 <div className="left col-lg-8 col-sm-12">
-                  <div className="d-flex align-items-center mt-5">
+                  <div className="d-flex align-items-center mt-5 mb-3">
                     <h4
                       id="eventIntro"
                       className="border-5 border-start border-primary px-2"
@@ -448,9 +448,42 @@ export default function Detail() {
                     </h4>
                   </div>
                   <HtmlRenderer htmlContent={eventInfo.content} />
+                  <div className="d-flex align-items-center mt-5">
+                  <h4
+                    id="eventIntro2"
+                    className="border-5 border-start border-primary px-2"
+                  >
+                    購買須知
+                  </h4>
+                </div>
+                <p className="lh-lg mt-4">
+                  1.本次活動購票進場採實名制，每位會員帳號限購2張票券。
+                  <br />
+                  2.若購買1張票券，該張票券填寫的參加者姓名須為購票會員帳號姓名，若購買2張票券，其中1張票券填寫的參加者姓名須為購票會員帳號姓名，請於購票前先至「會員專區」→「個人資料」確認會員姓名是否與會員本人證件姓名相符且完整填寫，恕不接受購票中及購票後修改，若經查核購票當下的「會員姓名」與填寫的「參加者姓名」無一符合者，主辦單位及遠大售票有權取消該會員於此活動購買之全數票券。若因事後修改會員資料，經核對後與購票當下資訊不符合者，主辦單位及遠大售票有權取消該會員於此活動購買之全數票券。(以上紅字部分為10/27新增實名制規定)
+                  <br />
+                  3.一個證件號僅能購買一張票券，請勿將個人資料提供給他人，以免影響自身購買權益。
+                  <br />
+                  4.系統列印票券時，將帶入購票頁面填寫的參加者資料，請確實填寫每位參加者的「身分證姓名」及「身分證證字號」，外籍人士請填寫「護照姓名」及「護照號碼」，購票時請務必確認每張票券對應之參加者資料填寫正確，以免影響入場權益。
+                  <br />
+                  5.實名制票券的參加者「姓名」及「證件字號」如填寫有誤將不接受任何形式變更資料，僅接受退票期限內申請退票，請務必確認再進行結帳。
+                  <br />
+                  6.詳細實施規則請見購票頁面說明，並建議提前加入會員，於「會員專區」→「個人資料」→「信用卡資訊」登錄信用卡完成驗證手續，以便進行購票流程。
+                </p>
+                <div className="d-flex align-items-center mt-5">
+                  <h4
+                    id="eventIntro3"
+                    className="border-5 border-start border-primary px-2"
+                  >
+                    使用方式
+                  </h4>
+                </div>
+                <p className="my-4">
+                  <i className="bi bi-dot" />
+                  入場專用QRCODE將會寄送到您的會員email，或請至會員中心＞訂單＞票卷內點擊出示使用。
+                </p>
                 </div>
                 {/* right bar */}
-                <div className="right d-none d-xxl-block col-3">
+                <div className="right d-none d-xxl-block col-4 pt-4 ps-5">
                   <div className="row seat1 mt-3">
                     <h5 className="col-12 mb-3">
                       {' '}
@@ -495,46 +528,7 @@ export default function Detail() {
                 </div>
               </section>
               {/* 購買須知 */}
-              <section className="left col-lg-8 col-sm-12">
-                <div className="d-flex align-items-center mt-5">
-                  <h4
-                    id="eventIntro2"
-                    className="border-5 border-start border-primary px-2"
-                  >
-                    購買須知
-                  </h4>
-                </div>
-                <p className="lh-lg mt-4">
-                  1.本次活動購票進場採實名制，每位會員帳號限購2張票券。
-                  <br />
-                  2.若購買1張票券，該張票券填寫的參加者姓名須為購票會員帳號姓名，若購買2張票券，其中1張票券填寫的參加者姓名須為購票會員帳號姓名，請於購票前先至「會員專區」→「個人資料」確認會員姓名是否與會員本人證件姓名相符且完整填寫，恕不接受購票中及購票後修改，若經查核購票當下的「會員姓名」與填寫的「參加者姓名」無一符合者，主辦單位及遠大售票有權取消該會員於此活動購買之全數票券。若因事後修改會員資料，經核對後與購票當下資訊不符合者，主辦單位及遠大售票有權取消該會員於此活動購買之全數票券。(以上紅字部分為10/27新增實名制規定)
-                  <br />
-                  3.一個證件號僅能購買一張票券，請勿將個人資料提供給他人，以免影響自身購買權益。
-                  <br />
-                  4.系統列印票券時，將帶入購票頁面填寫的參加者資料，請確實填寫每位參加者的「身分證姓名」及「身分證證字號」，外籍人士請填寫「護照姓名」及「護照號碼」，購票時請務必確認每張票券對應之參加者資料填寫正確，以免影響入場權益。
-                  <br />
-                  5.實名制票券的參加者「姓名」及「證件字號」如填寫有誤將不接受任何形式變更資料，僅接受退票期限內申請退票，請務必確認再進行結帳。
-                  <br />
-                  6.詳細實施規則請見購票頁面說明，並建議提前加入會員，於「會員專區」→「個人資料」→「信用卡資訊」登錄信用卡完成驗證手續，以便進行購票流程。
-                </p>
-              </section>
-              {/* 使用方式 */}
-              <section className="left col-lg-8 col-sm-12">
-                <div className="d-flex align-items-center mt-5">
-                  <h4
-                    id="eventIntro3"
-                    className="border-5 border-start border-primary px-2"
-                  >
-                    使用方式
-                  </h4>
-                </div>
-                <p className="my-4">
-                  <i className="bi bi-dot" />
-                  入場專用QRCODE將會寄送到您的會員email，或請至會員中心＞訂單＞票卷內點擊出示使用。
-                </p>
-              </section>
 
-              {/* 推薦活動 */}
               <section className="d-none d-xxl-inline-flex">
                 <motion.div
                   initial={{ y: 20, opacity: 0 }}
@@ -678,10 +672,8 @@ export default function Detail() {
 
           .object-fit-cover {
             width: 100%;
-             {
-              /* height: 500px; */
-            }
             object-fit: cover;
+            border-radius: 10px;
           }
 
           .wrapper {
@@ -698,7 +690,7 @@ export default function Detail() {
 
           .seat1 {
             color: white;
-            background: #686868c8;
+            background: var(--bg-gray-secondary-color);
             padding: 15px;
             border-radius: 10px;
           }
@@ -717,16 +709,15 @@ export default function Detail() {
             margin-left: 300px;
           }
           .left {
-             {
-              /* max-width: 70%; */
-            }
-            margin-right: 20px;
+             img{
+              max-width: 100%;
+             }
           }
           .right {
-             {
-              /* max-width: 30%; */
-            }
-            margin: 40px auto;
+            position: sticky;
+            top: 100px;
+            {/* margin: 40px auto; */}
+            height: 320px;
           }
            {
             /* .avatar {
